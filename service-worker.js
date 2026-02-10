2b226e144d30634d713dfc79142d79c59b691ea2
ownhong12-hue <ownhong12@gmail.com>
2026-02-11 04:59:58 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'gongbu-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/rivals.html',
+  '/study-log.html',
+  '/quiz-maker.html',
+  '/learning-materials.html',
+  '/api.js',
+  '/schools_data.js',
+  '/universities_data.js'
+];
+
+// 설치
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('캐시 열림');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 활성화
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('오래된 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 네트워크 요청 가로채기
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 캐시에 있으면 반환, 없으면 네트워크 요청
+        return response || fetch(event.request);
+      })
+  );
+});
